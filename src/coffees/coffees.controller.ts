e688077ce1c0af3d2ed26fa7a8381bf2ae7d3b60
Kainy Ryu <kainy2109@gmail.com>
2021-07-12 01:00:55 +0900

    @Res() allows to use express methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'Get all coffees';
+  findAll(@Res() response) {
+    response.status(201).send('Get all coffees');
+    // return 'Get all coffees';
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This is returning #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body('username') body) {
+    return body.username;
   }
 }
