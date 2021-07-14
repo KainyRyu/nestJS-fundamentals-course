f3a1e229df1caf9d2039772b0b17505902aa8647
Kainy Ryu <kainy2109@gmail.com>
2021-07-14 23:56:16 +0900

    Create Coffee Entity

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,14 @@
+import { Coffee } from './entities/coffee.entity';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'new coffee',
+      brand: 'Starbucks',
+      flavour: 'bitter',
+    },
+  ];
+}
