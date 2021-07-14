18100aa38c084834b8dcdbeea6d6dd064a95ef7f
Kainy Ryu <kainy2109@gmail.com>
2021-07-15 00:14:07 +0900

    Link service to controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,28 +15,27 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Res() response) {
-    response.status(201).send('Get all coffees');
-    // return 'Get all coffees';
+  findAll() {
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This is returning #${id} coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
-  create(@Body('username') body) {
-    return body.username;
+  create(@Body() body) {
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id, @Body() body) {
-    return `Update #${id} as ${body}`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
-  delete(@Param('id') id) {
-    return `Delete #${id}`;
+  delete(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
   }
 }
