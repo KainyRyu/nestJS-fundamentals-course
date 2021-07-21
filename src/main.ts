02f59db3a166c5d9917b45689ad07d1b4ec2a91c
Kainy RYU <kainy2109@gmail.com>
2021-07-21 22:58:13 +0900

    Handling malicious request data

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // will strip validated(returned) object of any properties that do not use any validation decorators
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
