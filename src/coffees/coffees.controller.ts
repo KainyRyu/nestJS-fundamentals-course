5a4e555d5493549132ad2d0fe6976432223e0878
Kainy RYU <kainy2109@gmail.com>
2021-07-21 22:15:58 +0900

    Introduction to Data Transfer Object:DTO)

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,6 @@
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import {
   Body,
   Controller,
@@ -25,13 +27,13 @@ export class CoffeesController {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.coffeesService.create(body);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id, @Body() body) {
-    return this.coffeesService.update(id, body);
+  update(@Param('id') id, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -2,5 +2,5 @@ export class Coffee {
   id: number;
   name: string;
   brand: string;
-  flavour: string;
+  flavour: string[];
 }
